6f83992e22b0cc2f99a194caff0e7e022fc95e1d
ClaraNocilla <claranocilla12@gmail.com>
2022-05-30 18:13:56 +0200

    prova controller

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -4,7 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Beer;
+
 class BeerController extends Controller
 {
-    //
+    public function index(){
+   
+     
+
+         $beer::all();
+
+            return view('home');
+}
 }
